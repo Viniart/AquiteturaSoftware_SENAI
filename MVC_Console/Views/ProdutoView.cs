a8ef7ee6ce0b3df4fb1a84437c24c73a0e1c77e1
Viniart <vinicioart4@hotmail.com>
2021-01-06 14:10:49 -0300

    First Commit

---
+++ MVC_Console/Views/ProdutoView.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using MVC_Console.Models;
+
+namespace MVC_Console.Views
+{
+    public class ProdutoView
+    {
+        
+        public void Listar(List<Produto> produtos)
+        {
+            foreach (var item in produtos)
+            {
+                Console.WriteLine($"Código: {item.Codigo}");
+                Console.WriteLine($"Produto: {item.Nome}");
+                Console.WriteLine($"Preco: R$ {item.Preco}");
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
